05ae407c1ab18ce9cd76fb1368887481253e2113
will.he <will.he@tech-trans.com>
2021-02-21 17:52:09 +0800

    add two numbers

--- src/xyz/gzzh/leetcode/application/AddTwoNumbers.java
+++ src/xyz/gzzh/leetcode/application/AddTwoNumbers.java
@@ -6,7 +6,8 @@ public class AddTwoNumbers {
 
         ListNode l2 = new ListNode(7, new ListNode(8, new ListNode(9, null)));
 
-        addTwoNumbers(l1, l2);
+        ListNode listNode = addTwoNumbers(l1, l2);
+        System.out.println(listNode);
 
     }
 
@@ -16,21 +17,40 @@ public class AddTwoNumbers {
             throw new RuntimeException("l1 or l2 must be not null");
         }
 
-        String dataOne = "";
-        while (l1 != null) {
-            dataOne = dataOne.concat(String.valueOf(l1.val));
-            l1 = l1.next;
+        ListNode result = null;
+        ListNode tail = null;
+        int in = 0;
+
+        while (l1 != null || l2 != null) {
+            int n1 = 0;
+            int n2 = 0;
+
+            if (l1 != null) {
+                n1 = l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                n2 = l2.val;
+                l2 = l2.next;
+            }
+
+            int sum = n1 + n2 + in;
+            int thisNodeVal = sum % 10;
+            in = sum / 10;
+
+            if (result == null) {
+                result = tail = new ListNode(thisNodeVal);
+            } else {
+                tail = tail.next = new ListNode(thisNodeVal);
+            }
         }
 
-        String dataTwo = "";
-
-        while (l2 != null) {
-            dataTwo = dataTwo.concat(String.valueOf(l2.val));
-            l2 = l2.next;
+        if (in > 0) {
+            tail.next = new ListNode(in);
         }
 
-        System.out.println(dataOne + "  " + dataTwo);
-        return null;
+        return result;
     }
 
     public static class ListNode {
@@ -64,32 +84,5 @@ public class AddTwoNumbers {
         public void setNext(ListNode next) {
             this.next = next;
         }
-    }
-
-    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
-        ListNode head = null, tail = null;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int n1 = l1 != null ? l1.val : 0;
-            int n2 = l2 != null ? l2.val : 0;
-            int sum = n1 + n2 + carry;
-            if (head == null) {
-                head = tail = new ListNode(sum % 10);
-            } else {
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next;
-            }
-            carry = sum / 10;
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
-        }
-        if (carry > 0) {
-            tail.next = new ListNode(carry);
-        }
-        return head;
     }
 }
