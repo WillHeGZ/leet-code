d9319a5f78ccc7237031ee79ff3916fc79299a38
Will He <hwh2015@qq.com>
2021-03-05 10:24:41 +0800

    collection demo

--- src/xyz/gzzh/leetcode/learning/CollectionDemo.java
+++ src/xyz/gzzh/leetcode/learning/CollectionDemo.java
@@ -5,14 +5,23 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CollectionDemo {
     static List<String> arrayList = new ArrayList<>();
+    static List<String> vectorArrayList = new Vector<>();
     static List<String> synchronizedArrayList = Collections.synchronizedList(new ArrayList<>());
     static List<String> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
 
     static Set<String> hashSet = new HashSet<>();
 
     public static void main(String[] args) {
-        CopyOnWriteArrayListSafe();
-        hashSet.add("c");
+        VectorArrayListSafe(vectorArrayList);
+    }
+
+    private static void VectorArrayListSafe(List<String> vectorArrayList) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                vectorArrayList.add(UUID.randomUUID().toString());
+                System.out.println(vectorArrayList);
+            }, "thread name : " + i).start();
+        }
     }
 
     private static void CopyOnWriteArrayListSafe() {
