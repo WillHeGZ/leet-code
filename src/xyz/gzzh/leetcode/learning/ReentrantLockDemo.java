3ecda30ee05f4f40e1791b93bd316fddf749daa8
will.he <will.he@tech-trans.com>
2021-03-06 16:59:36 +0800

    reentrant lock demo

---
+++ src/xyz/gzzh/leetcode/learning/ReentrantLockDemo.java
@@ -0,0 +1,78 @@
+package xyz.gzzh.leetcode.learning;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo {
+    static class SynchronizedObject implements Runnable {
+        public synchronized void sendSMS() {
+            System.out.println(Thread.currentThread().getName() + "\t invoked send SMS");
+            sendEMail();
+        }
+
+        protected synchronized void sendEMail() {
+            System.out.println(Thread.currentThread().getName() + "\tinvoked send E-mail");
+        }
+
+        @Override
+        public void run() {
+            sendSMS();
+        }
+    }
+
+    static class ReentrantLockObject {
+        ReentrantLock reentrantLock = new ReentrantLock();
+
+        public void sendSMS() {
+            reentrantLock.lock();
+            reentrantLock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t invoked send SMS");
+                sendEMail();
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                reentrantLock.unlock();
+            }
+
+        }
+
+        protected void sendEMail() {
+            reentrantLock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\tinvoked send E-mail");
+            } finally {
+                reentrantLock.unlock();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+/*
+        SynchronizedObject object = new SynchronizedObject();
+
+        new Thread(() -> {
+            object.sendSMS();
+        }, "a").start();
+        new Thread(() -> {
+            object.sendSMS();
+        }, "b").start();
+*/
+        ReentrantLockObject object = new ReentrantLockObject();
+
+        new Thread(() -> {
+            object.sendSMS();
+        }, "a").start();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        new Thread(() -> {
+            object.sendSMS();
+        }, "b").start();
+
+
+
+    }
+}
