4b5ec2d39b7e87ad7eb1bbdc24f3e12a95fe4c5a
will.he <will.he@tech-trans.com>
2021-03-30 22:21:43 +0800

    Two_10_DirectMemoryOOM

---
+++ src/xyz/gzzh/leetcode/learning/understandingthejvm/Two_9_JavaMethodAreaOOM.java
@@ -0,0 +1,9 @@
+package xyz.gzzh.leetcode.learning.understandingthejvm;
+
+public class Two_9_JavaMethodAreaOOM {
+    public static void main(String[] args) {
+        while (true) {
+//            Enhancer
+        }
+    }
+}
