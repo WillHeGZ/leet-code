a8d1f47e2d77f6bcfb0afd96b98fc38a77094c69
Will He <hwh2015@qq.com>
2021-04-14 11:30:07 +0800

    LengthOfLongestSubstring

---
+++ src/xyz/gzzh/leetcode/application/LengthOfLongestSubstring.java
@@ -0,0 +1,49 @@
+package xyz.gzzh.leetcode.application;
+
+/*
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 示例 1:
+ *    输入: s = "abcabcbb"
+ *    输出: 3
+ *    解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ *
+ *
+ * */
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        String s = " ";
+        int i = lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int maxLength = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            String headStr = "";
+            int count = 0;
+            for (int j = i; j < chars.length; j++) {
+                if (!headStr.contains(String.valueOf(chars[j]))) {
+                    count++;
+                } else {
+                    break;
+                }
+                headStr = headStr + chars[j];
+            }
+
+            if (count > maxLength) {
+                maxLength = count;
+            }
+        }
+
+        return maxLength;
+    }
+}
