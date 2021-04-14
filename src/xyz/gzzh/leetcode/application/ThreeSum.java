db1a57a1caa39187be9e9a38be899d7b027be26e
Will He <hwh2015@qq.com>
2021-04-14 12:17:15 +0800

    ThreeSum

---
+++ src/xyz/gzzh/leetcode/application/ThreeSum.java
@@ -0,0 +1,27 @@
+package xyz.gzzh.leetcode.application;
+
+import java.util.List;
+
+/*
+*
+* https://leetcode-cn.com/problems/3sum/
+*
+* 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+* 注意：答案中不可以包含重复的三元组。
+*
+* 输入：nums = [-1,0,1,2,-1,-4]
+* 输出：[[-1,-1,2],[-1,0,1]]
+* */
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+
+        return null;
+    }
+}
